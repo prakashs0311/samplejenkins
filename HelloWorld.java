0892bcaca5be59dc72d3092cdcb81a49f2cc2b64
prakashs0311 <71212904+prakashs0311@users.noreply.github.com>
2020-10-06 19:16:11 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	  System.out.println("Hi this is jenkins Application");
 	  System.out.println("webhook triggered");
+	  System.out.println("test triggered latest svn code");
 	}
 }
